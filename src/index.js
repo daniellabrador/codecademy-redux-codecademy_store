4fa16d4b01c272ae9994a5428acd1352d4b58b5c
Daniel Labrador <thonydaniellelabrador@gmail.com>
2021-10-05 01:14:25 +0800

    feat: complete Cart slice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { App } from './app/App.js';
+// Import the store here.
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app')
-);
+// Pass state and dispatch props to the <App /> component.
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+    <App 
+      
+    />
+    </React.StrictMode>,
+    document.getElementById('app')
+  )
+};
+render();
+
+// Subscribe render to the store.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
